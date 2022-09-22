5e203af3e179f5430ff1d73edc60355b3406b48f
Hacker2the20 <78942065+Hacker2the20@users.noreply.github.com>
2022-09-22 02:40:00 -0700

    Refactor

--- test/mainTests.js
+++ test/mainTests.js
@@ -11,4 +11,9 @@ describe('Main Suite', function(){
         assert.equal(helloWorldText, 'hello world')
 
     });
+
+    it('Validate data type', function(){
+        assert.typeOf(helloWorldText, 'string' ,'The expected data type is string')
+
+    });
 });
